1cfa2e11af4d8952839918a7fc3b55aebc395808
Daniel Vogel <54858106+vogelcodes@users.noreply.github.com>
2024-02-17 18:10:39 +0000

    railway gunicorn

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -33,8 +33,9 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 if 'CODESPACE_NAME' in os.environ:
     codespace_name = os.getenv("CODESPACE_NAME")
     codespace_domain = os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")
-    CSRF_TRUSTED_ORIGINS = [f'https://{codespace_name}-8000.{codespace_domain}','https://django-server-production-b812.up.railway.app']
-CSRF_TRUSTED_ORIGINS = ['https://django-server-production-b812.up.railway.app']
+    CSRF_TRUSTED_ORIGINS = [f'https://{codespace_name}-8000.preview.{codespace_domain}']
+else:
+    CSRF_TRUSTED_ORIGINS = ['https://django-server-production-b812.up.railway.app']
 
 # Application definition
 
