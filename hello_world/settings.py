003f415cd41803a67bdbb1f0ece07e2f2040814f
Daniel Vogel <54858106+vogelcodes@users.noreply.github.com>
2024-02-17 14:21:13 +0000

    railway CSRF host

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 if 'CODESPACE_NAME' in os.environ:
     codespace_name = os.getenv("CODESPACE_NAME")
     codespace_domain = os.getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")
-    CSRF_TRUSTED_ORIGINS = [f'https://{codespace_name}-8000.{codespace_domain}']
+    CSRF_TRUSTED_ORIGINS = [f'https://{codespace_name}-8000.{codespace_domain}','https://django-server-production-b812.up.railway.app']
 
 # Application definition
 
